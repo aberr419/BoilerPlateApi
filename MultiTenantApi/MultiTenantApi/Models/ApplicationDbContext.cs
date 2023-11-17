0e6b27db2f42ee648dcdbb640dd64bf2338d4c5a
ajb419 <aberridge419@gmail.com>
2023-11-17 08:49:12 -0700

    main/develop - add DbContext: connection string added to appSettings and middleware configured to use sql server

---
+++ MultiTenantApi/MultiTenantApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MultiTenantApi.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
